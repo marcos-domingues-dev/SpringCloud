34faaacb1afac86719a15ab338fed9fced633db4
Marcos Domingues <mdomingues.ti@gmail.com>
2020-09-13 14:17:12 -0300

    Spring Cloud - Aula 04: Load Balancer com @LoadBalanced / RestTemplate / Ribbon

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -29,10 +29,22 @@ public class LojaApplication {
 	 *   	Nas requisições REST que implementamos, passamos apenas o nome da outra aplicação: fornecedor
 	 *   	O Spring substitui automaticamente o nome pelo IP. 
 	 * --------------------------------------------------------------------------------
+	 * 
+	 * 	@LoadBalanced 
+	 * 		- Client Side Load Balancing (CSLB)
+	 * 		- É o cliente HTTP que decide qual microsserviço recebe a requisição
+	 *		- Como implementação do CSLB, usamos um projeto chamado Ribbon, que 
+	 *			faz parte do Spring Cloud Netflix 
+	 *
+	 * --------------------------------------------------------------------------------
+	 * 
+	 * Spring Feign
+	 * 		- Como alternativa ao RestTemplate, podemos usar o Spring Feign, que já usa o Ribbon para CSLB
+	 * 
 	 * */
 	
 	@Bean
-	@LoadBalanced
+	@LoadBalanced // -> Aqui está o Ribbon
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,13 @@ import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 	
 	@Autowired
-	RestTemplate restTemplate; // -> @Bean -> LojaApplication.getRestTemplate()
+	private RestTemplate restTemplate; // -> @Bean -> LojaApplication.getRestTemplate()
 
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
+	
+	
 	public void realizaCompra(CompraDTO compra) {				
 		ResponseEntity<InfoFornecedorDTO> exchange = 
 			restTemplate.exchange(
@@ -24,6 +30,14 @@ public class CompraService {
 				InfoFornecedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
+		
+		
+		// --> Para visualizar as instâncias, é preciso rodar uma segunda em outra porta:
+		// --> Run configurations > arguments > program arguments: "--server.port=8082"
+		
+		eurekaClient.getInstances("fornecedor").stream()
+			.forEach(fornecedor -> 
+			System.out.println("http://localhost:"+fornecedor.getPort()));
 	}
 
 }
