0fb944f4fe4a7553a820bcddbd44bbfb9265a99a
Marcos Domingues <mdomingues.ti@gmail.com>
2020-09-14 08:27:22 -0300

    Spring Cloud - Aula 05: Distributed Tracing e Spring Sleuth

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.fornecedor.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,14 @@ import br.com.alura.microservice.fornecedor.repository.InfoRepository;
 
 @Service
 public class InfoService {
+	
+	private static final Logger log = LoggerFactory.getLogger(InfoService.class);
 
 	@Autowired
 	InfoRepository infoRepository;
 	
 	public InfoFornecedor getInfoPorEstado(String estado) {
+		log.info("informações buscadas");
 		return infoRepository.findByEstado(estado);		
 	}
 
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,18 @@ import br.com.alura.microservice.loja.modelo.Compra;
 @Service
 public class CompraService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	FornecedorClient fornecedorClient;
 	
 	public Compra realizaCompra(CompraDTO compra) {
+		final String estado = compra.getEndereco().getEstado();
+				
+		LOG.info("buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 		
 		Compra compraSalva = new Compra();
